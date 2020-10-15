488c90f21fa61d3c66d086f311dd29ed60a2d409
Division01 <vincent.fischer@hotmail.com>
2020-10-15 18:07:24 +0200

    kk

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,12 +10,20 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (i *spaceEraser) Read(b []byte) (n int, err error) {
+	n, err = i.r.Read(b)
+	var toReturn = []byte{}
+	for _, char := range b {
+		if char != 32 {
+			toReturn = append(toReturn, char)
+		}
+	}
+	copy(b, toReturn)
+	return n, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
 	io.Copy(os.Stdout, &r)
 }
-
-// Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
-// modifying the stream by removing the spaces.
-// Expected output: "Helloworld!"
